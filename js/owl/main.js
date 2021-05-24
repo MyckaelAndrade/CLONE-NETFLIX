2dba7d4e38ecf99e49e5af286bae6b76d922c04a
MyckaelAndrade <myckael.and@gmail.com>
2021-05-24 18:11:46 -0300

    iniat commit
    alteração

---
+++ js/owl/main.js
@@ -0,0 +1,26 @@
+function load() {
+    alert("Página Carregada");
+}
+
+
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:true
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:true
+        }
+    }
+})
